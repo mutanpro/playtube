ad2f06e058dd46e87e52f51a14d06b894f188d33
mutanNA <mutan@unisnu.ac.id>
2026-04-13 10:55:21 +0700

    fix

--- sources/logout/content.php
+++ sources/logout/content.php
@@ -14,7 +14,7 @@ if (isset($_COOKIE['user_id'])) {
 	$db->where('session_id', PT_Secure($_COOKIE['user_id']));
 	$db->delete(T_SESSIONS);
     unset($_COOKIE['user_id']);
-    setcookie('user_id', null, -1,'/');
+    setcookie('user_id', '', -1,'/');
 } 
 $_SESSION = array();
 unset($_SESSION);
